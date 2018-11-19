1dded871fc013b480c60d1862b69adec35bfec9e
Vladimir Karpovich <dev.karpovich@gmail.com>
2018-11-19 14:09:33 +0700

    fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -67,7 +67,7 @@
         <div class="flex-center position-ref full-height">
             <div class="content">
                 <div class="title m-b-md">
-                    {{ $review->text }}
+                    {{ $review->text ?? '' }}
                 </div>
             </div>
         </div>
